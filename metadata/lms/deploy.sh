64596a22e65aff06a89a276ee6b39a66c9d63ac2
Joe Fitzgerald <jfitzgerald@vmware.com>
2023-08-21 15:36:11 -0600

    normalize deployment script to permit local deployment
    Signed-off-by: Joe Fitzgerald <jfitzgerald@vmware.com>

--- metadata/lms/deploy.sh
+++ metadata/lms/deploy.sh
@@ -1,26 +1,49 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 
-DIR=$(dirname $0)
+DIR="$(dirname "$0")"
 
 ci-utils() {
-    docker run --rm -i -v $(pwd):$(pwd) registry.gitlab.com/gitlab-ci-utils/curl-jq $@
+    if [[ "${PENGUIN_USEDOCKER:-true}" != "false" ]]; then
+        docker run --rm -i -v "$(pwd)":"$(pwd)" badouralix/curl-jq "$@"
+    else
+        $@
+    fi
 }
 
 penguinctl-docker() {
-    docker run --rm -v $(pwd):$(pwd) ghcr.io/vmware-tanzu-learning/penguinctl --url=${PENGUINCTL_APIURL} --token=${PENGUINCTL_APITOKEN} $@
+    echo "penguinctl version $(docker run --rm ghcr.io/vmware-tanzu-learning/penguinctl:latest --version)"
+    docker run --rm -v "$(pwd)":"$(pwd)" ghcr.io/vmware-tanzu-learning/penguinctl:latest --url="${PENGUINCTL_APIURL}" --token="${PENGUINCTL_APITOKEN}" $@
+}
+
+penguinctl-local() {
+    echo "penguinctl version $(penguinctl --version)"
+    penguinctl --url="${PENGUINCTL_APIURL}" --token="${PENGUINCTL_APITOKEN}" $@
+}
+
+penguinctlcmd() {
+    if [[ "${PENGUIN_USEDOCKER:-true}" != "false" ]]; then
+        penguinctl-docker "$@"
+    else
+        penguinctl-local "$@"
+    fi
 }
 
 deploy-all() {
-
-    export GIT_STATUS="$(git status | ci-utils jq -R -s '.' | sed 's:^.\(.*\).$:\1:' )"
+    GIT_STATUS="$(git status | ci-utils jq -R -s '.' | sed 's:^.\(.*\).$:\1:' )"
+    export GIT_STATUS
     export GITHUB_REF_NAME="${GITHUB_REF_NAME:-$(git branch --show-current)}"
     export GITHUB_SHA="${GITHUB_SHA:-$GIT_STATUS}"
     export GITHUB_ACTOR="${GITHUB_ACTOR:-${USER}@local}"
-    export DEBUG_DATE=$(date)
+    DEBUG_DATE=$(date)
+    export DEBUG_DATE
 
-    envsubst < ${DIR}/course.template.json > ${DIR}/course.json
-    penguinctl-docker apply course -f $(pwd)/${DIR}/course.json
-    rm ${DIR}/course.json
+    envsubst < "${DIR}/course.template.json" > "${DIR}/course.json"
+    penguinctlcmd apply course -f "$(pwd)/${DIR}/course.json"
+    rm "${DIR}/course.json"
 }
 
 "$@"
